44cef301dc821c0730fc59bc9d6edb27d9ad13e3
aundere <58791453+aundere@users.noreply.github.com>
2025-09-04 01:12:04 +0200

    Implement output file handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io::stdout;
+
+use crate::types::stringify_config;
+
 mod parser;
 mod types;
 
@@ -5,8 +9,15 @@ fn main() {
     let file_name = std::env::args().nth(1)
         .expect("Please provide a file name as the first argument");
 
+    let output_file_name = std::env::args().nth(2)
+        .expect("Please provide an output file name as the second argument");
+
     let file = std::fs::read(file_name)
         .expect("Unable to read the file");
+
+    let output_file = std::fs::File::create(output_file_name)
+        .expect("Unable to create the output file");
+    let mut output_stream = std::io::BufWriter::new(output_file);
 
     let content = String::from_utf8(file)
         .expect("Unable to convert file to string");
@@ -15,9 +26,11 @@ fn main() {
 
     match result {
         Ok((remaining, config)) => {
-            println!("Parsed config: {:#?}", config);
-            if !remaining.trim().is_empty() {
-                eprintln!("Warning: Unparsed content remaining: {}", remaining);
+            stringify_config(&config, &mut stdout()).expect("Failed to write output");
+            stringify_config(&config, &mut output_stream).expect("Failed to write output");
+
+            if !remaining.is_empty() {
+                println!("Warning: Unparsed content remaining: {}", remaining);
             }
         }
         Err(err) => eprintln!("Failed to parse config: {:?}", err),
--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 /// Represents the possible values a property can have in a BLK configuration.
 #[derive(Debug, PartialEq)]
 pub enum BlkPropertyValue {
@@ -42,4 +44,63 @@ pub struct BlkBlock {
 #[derive(Debug, PartialEq)]
 pub struct BlkConfig {
     pub block: BlkBlock
+}
+
+/// Ugly function to convert a BLK configuration into a string representation.
+pub fn stringify_config(config: &BlkConfig, writer: &mut dyn Write) -> Result<(), std::io::Error> {
+    fn stringify_config_inner(writer: &mut dyn Write, entry: &BlkEntry, recurse_step: i32) -> Result<(), std::io::Error> {
+        write!(writer, "{}", &"    ".repeat(recurse_step as usize))?;
+
+        match entry {
+            BlkEntry::Section(section) => {
+                write!(writer, "{}{{\n", section.name)?;
+
+                for entry in &section.entries {
+                    stringify_config_inner(writer, entry, recurse_step + 1)?;
+                }
+
+                write!(writer, "{}}}\n", &"    ".repeat(recurse_step as usize))?;
+            },
+            BlkEntry::Property(property) => {
+                write!(writer, "{}", property.key)?;
+
+                match &property.value {
+                    BlkPropertyValue::Text(text) => {
+                        write!(writer, ":t=\"{}\"", text)?;
+                    },
+                    BlkPropertyValue::Boolean(boolean) => {
+                        write!(writer, ":b={}", if *boolean { "yes" } else { "no" })?;
+                    },
+                    BlkPropertyValue::Integer(integer) => {
+                        write!(writer, ":i={}", integer)?;
+                    },
+                    BlkPropertyValue::Real(real) => {
+                        write!(writer, ":r={}", real)?;
+                    },
+                    BlkPropertyValue::Vector2(x, y) => {
+                        write!(writer, ":p2={}, {}", x, y)?;
+                    },
+                    BlkPropertyValue::Vector3(x, y, z) => {
+                        write!(writer, ":p3={}, {}, {}", x, y, z)?;
+                    },
+                    BlkPropertyValue::Vector4(x, y, z, w) => {
+                        write!(writer, ":p4={}, {}, {}, {}", x, y, z, w)?;
+                    },
+                    BlkPropertyValue::Color(r, g, b, a) => {
+                        write!(writer, ":c={}, {}, {}, {}", r, g, b, a)?;
+                    }
+                }
+
+                write!(writer, "\n")?;
+            }
+        }
+
+        Ok(())
+    }
+
+    for entry in &config.block.entries {
+        stringify_config_inner(writer, entry, 0)?;
+    }
+
+    Ok(())
 }
